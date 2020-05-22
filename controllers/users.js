d72b7e6d3783b3b9cff3654eeac2081d32b5353d
Terrell Lemons <lemonsterrell43@gmail.com>
2020-05-22 13:53:59 -0700

    Ira's new commit

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,8 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+// Add the SECRET
+const SECRET = process.env.SECRET;
 
 module.exports = {
   signup
@@ -8,10 +12,24 @@ async function signup(req, res) {
   const user = new User(req.body);
   try {
     await user.save();
+
+    const token = createJWT(user)
+
     // TODO: Send back a JWT instead of the user
-    res.json(user);
+    res.json({ token });
   } catch (err) {
+
     // Probably a duplicate email
     res.status(400).json(err);
   }
+}
+
+/*----- Helper Functions -----*/
+
+function createJWT(user) {
+  return jwt.sign(
+    { user }, // data payload
+    SECRET,
+    { expiresIn: '24h' }
+  );
 }
